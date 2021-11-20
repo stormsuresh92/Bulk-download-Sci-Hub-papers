48dc09568422762d72f3a33512ef73c43de9022b
stormsuresh92 <38948398+stormsuresh92@users.noreply.github.com>
2021-11-20 12:23:06 +0530

    Add files via upload

---
+++ SciHub_PDF_Automate_By_PMIDs.py
@@ -0,0 +1,56 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+import os
+from tqdm import tqdm
+import logging
+
+
+
+header = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36',
+    'Connection':'keep-alive',
+    'Keep-Alive': 'timeout=60'
+}
+
+logging.basicConfig(filename='Logfile.log', level=logging.DEBUG, 
+                    format='%(asctime)s-%(message)s', datefmt='%d-%b-%y %H-%M-%S')
+
+cur_dir = os.getcwd()
+pdf = cur_dir + '/Downloaded PDFs'
+if not os.path.exists(pdf):
+    os.mkdir(pdf)
+    
+print('******************************')
+print('STARTING TO WRITE PDF FILES...')
+file = open('Input.txt', 'r')
+pmids= file.readlines()
+for pmid in tqdm(pmids):
+
+    try:
+        payload={
+            'sci-hub-plugin-check':'',
+            'request':str(pmid.strip())
+        }
+        base_url = 'https://sci-hub.se/'
+        r = requests.post(base_url, headers=header, data=payload)
+        soup = BeautifulSoup(r.content, 'html.parser')
+        cont = soup.find(id='pdf').get('src').replace('#navpanes=0&view=FitH', '')
+        if not cont.startswith('https:'):
+            pdfurl = 'https:'+str(cont)
+        else:
+            pdfurl = cont
+        
+        res = requests.get(pdfurl, stream=True)
+        with open(pdf + '/' + pmid.strip() + '.pdf', 'wb') as f:
+            for chunk in res.iter_content(chunk_size=1024):
+                if chunk:
+                    f.write(chunk)         
+    except:
+        NoPDF=open('PDF_NOT_FOUND.txt', 'a')
+        NoPDF.write(pmid.strip()+'\n')
+    time.sleep(30)
+
+print('\n')
+print('Download completed')
+input()
---
+++ SciHub_PdfUrl_Extract_Final.py
@@ -0,0 +1,43 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+import datetime
+from tqdm import tqdm
+
+
+
+header = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36',
+    'Connection':'keep-alive'
+}
+
+print('STARTING EXTRACTION PROCESS...')
+file = open('Input.txt', 'r')
+dois = file.readlines()
+for doi in tqdm(dois):
+    time.sleep(0.02)
+    try:
+        payload={
+            'sci-hub-plugin-check':'',
+            'request':str(doi)
+        }
+        base_url = 'https://sci-hub.se/'
+        r = requests.post(base_url, headers=header, data=payload)
+        soup = BeautifulSoup(r.content, 'html.parser')
+        cont = soup.find(id='pdf').get('src').replace('#navpanes=0&view=FitH', '')
+        Pdfname = soup.find(id='pdf').get('src').replace('#navpanes=0&view=FitH', '').split('/')[-1].replace('.pdf', '')
+        if not cont.startswith('https:'):
+            cont = 'https:'+str(cont)
+        else:
+            cont = cont
+        #print('PDF Available', '=>', doi.strip(), '##', datetime.datetime.now())
+        output_file = open('PDFURLs.tsv', 'a')
+        output_file.write(doi.strip() + '\t' + str(cont) + '\t' + Pdfname + '\n')
+        output_file.close()
+    except:
+        #print('NoPDF', '=>', doi.strip(), '##', datetime.datetime.now())
+        output_file = open('No_Pdf.txt', 'a')
+        output_file.write(doi.strip() + '\n')   
+        output_file.close()
+    time.sleep(15)
+
